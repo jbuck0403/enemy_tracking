545282b1265930c006dc5e959958a036656d3ef3
Jack Buck <89940883+jbuck0403@users.noreply.github.com>
2024-11-18 17:27:42 -0800

    preparing to move shooting capability to interface

--- Assets/Scripts/Defender.cs
+++ Assets/Scripts/Defender.cs
@@ -1,3 +1,8 @@
 using UnityEngine;
 
-public class Defender : Enemy { }
+public class Defender : Enemy
+{
+
+    //ensures there are up to 3 shields - only one shield per character up to a max of 3 at once (per defender)
+    //cannot shield itself
+}
--- Assets/Scripts/Shield.cs
+++ Assets/Scripts/Shield.cs
@@ -1,13 +1,53 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Shield : Character
 {
+    [SerializeField]
+    private static readonly GameObject shieldPrefab;
+
     [NonSerialized]
     public Transform shieldFrom;
 
     [NonSerialized]
     public Transform shielding;
+
+    [HideInInspector]
+    [SerializeField]
+    protected new GameObject projectilePrefab = null;
+
+    public static Shield CreateShield(
+        Transform master,
+        Vector3 spawnPosition,
+        GameObject prefab = null
+    )
+    {
+        // Use provided prefab or try to load from Resources
+        prefab = prefab ?? shieldPrefab;
+
+        if (prefab == null)
+        {
+            Debug.LogError("Shield prefab not found!");
+            return null;
+        }
+
+        // Instantiate the shield
+        GameObject instance = Instantiate(prefab, spawnPosition, Quaternion.identity);
+        Shield shield = instance.GetComponent<Shield>();
+
+        if (shield == null)
+        {
+            Debug.LogError("Shield component not found on prefab!");
+            Destroy(instance);
+            return null;
+        }
+
+        // Setup the shield
+        shield.SetMaster(master);
+
+        return shield;
+    }
 
     public void SetMaster(Transform master)
     {
@@ -23,7 +63,7 @@ public class Shield : Character
         }
     }
 
-    protected override void FixedUpdate()
+    private void ShieldMovement()
     {
         if (shielding != null)
         {
@@ -33,6 +73,11 @@ public class Shield : Character
         {
             FaceTarget(shieldFrom);
         }
+    }
+
+    protected override void FixedUpdate()
+    {
+        ShieldMovement();
         base.FixedUpdate();
     }
 }

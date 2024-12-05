bdd6aa63b47044490cec065a1e3162171d3d90de
Jack Buck <89940883+jbuck0403@users.noreply.github.com>
2024-12-05 03:09:28 -0800

    updated enemies to space themselves out when too close

--- Assets/Scripts/Characters/Enemy.cs
+++ Assets/Scripts/Characters/Enemy.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 public class Enemy : Combatant
 {
+    [SerializeField]
+    private readonly float separationDistance = 1f;
+
+    [SerializeField]
+    private LayerMask shipLayer;
     private GameObject player;
     public bool IsDying { get; set; } = false;
 
@@ -49,6 +54,7 @@ public class Enemy : Combatant
 
         if (player != null)
         {
+            ApplyLateralForce();
             FollowTarget(player.transform);
         }
     }
@@ -58,6 +64,55 @@ public class Enemy : Combatant
         if (TargetWithinDetectionRange(player.transform))
         {
             FireProjectileWithRateOfFire();
+        }
+    }
+
+    protected bool IsShipTooClose(out Enemy nearbyEnemy)
+    {
+        nearbyEnemy = null;
+        Collider2D[] nearbyShips = Physics2D.OverlapCircleAll(
+            transform.position,
+            separationDistance,
+            shipLayer
+        );
+
+        foreach (var ship in nearbyShips)
+        {
+            if (ship.gameObject != gameObject) // Skip self
+            {
+                nearbyEnemy = ship.GetComponent<Enemy>();
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void ApplyLateralForce()
+    {
+        Enemy otherShip;
+        if (IsShipTooClose(out otherShip))
+        {
+            Vector2 lateralForce = Vector2.Perpendicular(transform.up);
+
+            // If the other ship is moving laterally, move in the opposite direction
+            Rigidbody2D otherRb = otherShip.GetComponent<Rigidbody2D>();
+            if (Mathf.Abs(otherRb.velocity.x) > 0.1f) // Check if other ship is moving laterally
+            {
+                if (otherRb.velocity.x > 0)
+                {
+                    lateralForce = -lateralForce; // Move left if other ship is moving right
+                }
+            }
+            else // Default to position-based if other ship isn't moving laterally
+            {
+                if (transform.position.x < player.transform.position.x)
+                {
+                    lateralForce = -lateralForce;
+                }
+            }
+
+            MoveInDirection(lateralForce);
         }
     }
 }
